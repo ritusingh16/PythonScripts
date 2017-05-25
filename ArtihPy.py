88e36f1b2600a88233817b356bf33b27c6d6ec50
Rahul Kawadkar <rahul.2605@yahoo.co.in>
2017-05-25 02:13:49 -0400

    User Input Example
    Simple Arithmetic operations using user inputs, for integers and strings

---
+++ ArtihPy.py
@@ -0,0 +1,16 @@
+var1 = int(input("Enter First Digit : "))
+print ("First digit is :", var1)
+var2 = int(input("Enter Second Digit : "))
+print ("Second digit is: ", var2)
+sum = var1+var2
+diff = var1-var2
+product = var1*var2
+div = var1/var2
+mod = var1%var2
+print("Sum of two digits is : " , sum)
+print("Difference of two digits is : " , diff)
+print("Product of two digits is : " , product)
+print("Division of two digits is : " , div)
+print("Modulo of two digits is : " , mod)
+str1 = input("Was the result correct? Enter 'Yes' or 'No'\n")
+print ("ThankYou, your response was: " + str1)
