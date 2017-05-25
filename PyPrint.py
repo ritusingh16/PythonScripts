f6f260bba66d20349f0b7db2deeb25e899914593
Rahul Kawadkar <rahul.2605@yahoo.co.in>
2017-05-25 01:34:42 -0400

    Update PyPrint.py
    adding rubbish print statements

--- PyPrint.py
+++ PyPrint.py
@@ -1,2 +1,11 @@
+#print ("Hello, Hello") this is comment
 print ("Hello, Hello")
 print ("Hello, to the World!!")
+print ("# This is not a comment. :P")
+a = 4
+print (a)
+print ("Value printed above is the value of a which is : " , a)
+b = 12+6
+print ("Value of b is is, " , b)
+c=a+b
+print ("Sum of a and b is," , c)
